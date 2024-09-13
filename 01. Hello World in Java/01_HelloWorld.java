c8776638e198ba28dc21073633c03c185f20e020
Sunil Kumar K V <sunilkvb44@gmail.com>
2024-09-14 01:35:29 +0530

    Update 01_HelloWorld.java

--- 01. Hello World in Java/01_HelloWorld.java
+++ 01. Hello World in Java/01_HelloWorld.java
@@ -1,6 +1,17 @@
+//  How to Print number like this
 class HelloWorld {
 	public static void main (String[] args)
 	{
 		System.out.println(12);
+	}
+}
+
+
+// Printing text in Java Like this
+class CodeChef
+{
+	public static void main (String[] args)
+	{
+		System.out.println("I love Java");
 	}
 }
